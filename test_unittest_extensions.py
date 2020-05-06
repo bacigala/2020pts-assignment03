fb6e28f187d54a99c977e834ef8e1a71236b03fb
Martin Stolarik <stolarik6@uniba.sk>
2020-05-06 23:02:31 +0200

    created test for 'complete_neighbourhood' function

--- test_unittest_extensions.py
+++ test_unittest_extensions.py
@@ -1,36 +1,63 @@
-import unittest
+from unittest import TestCase
+import asyncio
 from extensions import complete_neighbourhood
+
+rules = {}
 
 
 # factory of mocked get_neighbours_from_network (to omit networking)
 # @rules parameter is a dictionary where key is server port and value set of neighbours
-def get_neighbours_mocked_factory(rules):
-    def get_neighbours_mocked(node):
-        return rules.get(node)
+def get_neighbours_mocked_factory():
+    async def get_neighbours_mocked(node):
+        return set(rules.get(node))
 
     return get_neighbours_mocked
 
 
 # factory of mocked get_neighbours_from_network (to omit networking)
 # @rules parameter is a dictionary where key is server port and value set of neighbours
-def add_neighbour_mocked_factory(rules):
-    def add_neighbour_mocked(node, new_neighbour):
-        rules.get(node).add(new_neighbour)
+def add_neighbour_mocked_factory():
+    async def add_neighbour_mocked(node, new_neighbour):
+        new_neighbour_set = set(rules.get(node))
+        new_neighbour_set.add(new_neighbour)
+        rules.update({node: new_neighbour_set})
 
     return add_neighbour_mocked
 
 
-class TestExtensions(unittest.TestCase):
+# setup basic graph of nodes - represented as sets of neighbours
+def reset_rules():
+    global rules
+    rules = {
+        0: {1, 2, 3},
+        1: {0, 2},
+        2: {0, 1},
+        3: {0, 4},
+        4: {3},
+        5: {},
+        6: {7},
+        7: {6}
+    }
+
+
+class TestCompleteNeighbourhood(TestCase):
     def setUp(self):
-        self.rules = {
-            "0": {"1", "2", "3"},
-            "1": {"0", "2"},
-            "2": {"0", "1"},
-            "3": {"0", "4"},
-            "4": {"4"},
-            "5": {}
+        reset_rules()
+
+    def test_complete_neighbourhood(self):
+        expected_rules = {
+            0: {1, 2, 3},
+            1: {0, 2, 3},
+            2: {0, 1, 3},
+            3: {0, 1, 2, 4},
+            4: {3},
+            5: {},
+            6: {7},
+            7: {6}
         }
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(complete_neighbourhood(0, get_neighbours_mocked_factory(), add_neighbour_mocked_factory()))
 
-
-if __name__ == '__main__':
-    unittest.main()
+        for x in range(8):
+            self.assertTrue(set(rules.get(x)).issubset(set(expected_rules.get(x))))
+            self.assertTrue(set(expected_rules.get(x)).issubset(set(rules.get(x))))
