05d5258c04dac8283edb4001c19935a7231802e6
Martin Stolarik <stolarik6@uniba.sk>
2020-05-07 10:58:01 +0200

    created test for functions 'climb_degree' and 'distance4'

--- test_unittest_extensions.py
+++ test_unittest_extensions.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 import asyncio
+import threading
 from extensions import complete_neighbourhood
+from extensions import climb_degree
+from extensions import distance4
 
 rules = {}
 
@@ -25,26 +28,19 @@ def add_neighbour_mocked_factory():
     return add_neighbour_mocked
 
 
-# setup basic graph of nodes - represented as sets of neighbours
-def reset_rules():
-    global rules
-    rules = {
-        0: {1, 2, 3},
-        1: {0, 2},
-        2: {0, 1},
-        3: {0, 4},
-        4: {3},
-        5: {},
-        6: {7},
-        7: {6}
-    }
-
-
-class TestCompleteNeighbourhood(TestCase):
-    def setUp(self):
-        reset_rules()
-
+class TestExtensions(TestCase):
     def test_complete_neighbourhood(self):
+        global rules
+        rules = {
+            0: {1, 2, 3},
+            1: {0, 2},
+            2: {0, 1},
+            3: {0, 4},
+            4: {3},
+            5: {},
+            6: {7},
+            7: {6}
+        }
         expected_rules = {
             0: {1, 2, 3},
             1: {0, 2, 3},
@@ -56,8 +52,130 @@ class TestCompleteNeighbourhood(TestCase):
             7: {6}
         }
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(complete_neighbourhood(0, get_neighbours_mocked_factory(), add_neighbour_mocked_factory()))
+        loop.run_until_complete(
+            complete_neighbourhood(0, get_neighbours_mocked_factory(), add_neighbour_mocked_factory()))
 
         for x in range(8):
             self.assertTrue(set(rules.get(x)).issubset(set(expected_rules.get(x))))
             self.assertTrue(set(expected_rules.get(x)).issubset(set(rules.get(x))))
+
+    def test_climb_degree(self):
+        global rules
+        rules = {
+            0: {2},
+            1: {4, 5, 6, 7},
+            2: {0},
+            3: {4},
+            4: {1, 3, 5},
+            5: {1, 4, 6, 7},
+            6: {1, 5},
+            7: {1, 5}
+        }
+
+        def stop_loop(loop):
+            loop.stop()
+
+        def run_forever_safe(loop):
+            loop.run_forever()
+            loop_tasks_all = asyncio.all_tasks(loop=loop)
+
+            for task in loop_tasks_all: task.cancel()
+
+            for task in loop_tasks_all:
+                if not (task.done() or task.cancelled()):
+                    try:
+                        # wait for task cancellations
+                        loop.run_until_complete(task)
+                    except asyncio.CancelledError:
+                        pass
+            loop.close()
+
+        def await_sync(task):
+            while not task.done(): pass
+            return task.result()
+
+        loop = asyncio.new_event_loop()
+
+        # closures for running and stopping the event-loop
+        run_loop_forever = lambda: run_forever_safe(loop)
+        close_loop_safe = lambda: loop.call_soon_threadsafe(stop_loop, loop)
+
+        # dedicated thread for running the event loop
+        thread = threading.Thread(target=run_loop_forever)
+
+        tested_coroutine = asyncio.run_coroutine_threadsafe(climb_degree(3, get_neighbours_mocked_factory()), loop)
+
+        # begin the thread to run the event-loop
+        thread.start()
+
+        # synchronously wait for the result of tested task
+        result = await_sync(tested_coroutine)
+
+        # close the loop
+        close_loop_safe()
+        thread.join()
+
+        # verify test result
+        self.assertEqual(result, 1)
+
+    def test_distance4(self):
+        global rules
+        rules = {
+            0: {2},
+            1: {4, 5, 6, 7},
+            2: {0, 3},
+            3: {2, 4},
+            4: {1, 3, 5},
+            5: {1, 4, 6, 7},
+            6: {1, 5},
+            7: {1, 5}
+        }
+
+        def stop_loop(loop):
+            loop.stop()
+
+        def run_forever_safe(loop):
+            loop.run_forever()
+            loop_tasks_all = asyncio.all_tasks(loop=loop)
+
+            for task in loop_tasks_all: task.cancel()
+
+            for task in loop_tasks_all:
+                if not (task.done() or task.cancelled()):
+                    try:
+                        # wait for task cancellations
+                        loop.run_until_complete(task)
+                    except asyncio.CancelledError:
+                        pass
+            loop.close()
+
+        def await_sync(task):
+            while not task.done(): pass
+            return task.result()
+
+        loop = asyncio.new_event_loop()
+
+        # closures for running and stopping the event-loop
+        run_loop_forever = lambda: run_forever_safe(loop)
+        close_loop_safe = lambda: loop.call_soon_threadsafe(stop_loop, loop)
+
+        # dedicated thread for running the event loop
+        thread = threading.Thread(target=run_loop_forever)
+
+        tested_coroutine = asyncio.run_coroutine_threadsafe(distance4(0, get_neighbours_mocked_factory()), loop)
+
+        # begin the thread to run the event-loop
+        thread.start()
+
+        # synchronously wait for the result of tested task
+        result = await_sync(tested_coroutine)
+
+        # close the loop
+        close_loop_safe()
+        thread.join()
+
+        # verify test result
+        expected = {1, 5}
+        for x in range(8):
+            self.assertTrue(result.issubset(expected))
+            self.assertTrue(expected.issubset(result))
